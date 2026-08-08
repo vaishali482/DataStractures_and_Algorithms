08bfb79307e0797bf114e2c132b6795b4feb3248
Vaishali Gautam <105813818+vaishali482@users.noreply.github.com>
2026-08-08 22:58:46 +0530

    Time: 390 ms (5.11%), Space: 68.2 MB (11%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.py
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -2,8 +2,5 @@ import pandas as pd
 import numpy as np
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    qualifies = ((employees['employee_id'] % 2 != 0) & (~employees['name'].str.startswith('M')))
-    
-    employees['bonus'] = np.where(qualifies, employees['salary'], 0)
-
-    return employees[['employee_id', 'bonus']].sort_values(by='employee_id')
+    employees['bonus'] = np.select([(employees['employee_id'] % 2 == 1) & (employees['name'].str[0] != 'M')], [employees['salary']], default = 0)
+    return employees[['employee_id', 'bonus']].sort_values(by = 'employee_id', ascending = True)
